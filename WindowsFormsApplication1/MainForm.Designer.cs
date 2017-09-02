ceba830a6ba4010b6addd715e253cfd3c69b8a19
Ryan L. Boyd <username@users.noreply.github.com>
2017-09-02 11:09:15 -0500

    Initial commit

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.WordWindowSizeTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.ScanSubfolderCheckbox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PunctuationBox = new System.Windows.Forms.TextBox();
+            this.FilenameLabel = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.FunctionWordTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WordWindowSizeTextbox
+            // 
+            this.WordWindowSizeTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WordWindowSizeTextbox.Location = new System.Drawing.Point(271, 61);
+            this.WordWindowSizeTextbox.Name = "WordWindowSizeTextbox";
+            this.WordWindowSizeTextbox.Size = new System.Drawing.Size(53, 22);
+            this.WordWindowSizeTextbox.TabIndex = 0;
+            this.WordWindowSizeTextbox.Text = "3";
+            this.WordWindowSizeTextbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.WordWindowSizeTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.WordWindowSizeTextbox_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Word Window Size";
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // ScanSubfolderCheckbox
+            // 
+            this.ScanSubfolderCheckbox.AutoSize = true;
+            this.ScanSubfolderCheckbox.Location = new System.Drawing.Point(240, 285);
+            this.ScanSubfolderCheckbox.Name = "ScanSubfolderCheckbox";
+            this.ScanSubfolderCheckbox.Size = new System.Drawing.Size(108, 17);
+            this.ScanSubfolderCheckbox.TabIndex = 2;
+            this.ScanSubfolderCheckbox.Text = "Scan subfolders?";
+            this.ScanSubfolderCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(218, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.FolderBrowser.ShowNewFolderButton = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Characters to Remove from Texts:";
+            // 
+            // PunctuationBox
+            // 
+            this.PunctuationBox.AcceptsTab = true;
+            this.PunctuationBox.Location = new System.Drawing.Point(202, 118);
+            this.PunctuationBox.Name = "PunctuationBox";
+            this.PunctuationBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.PunctuationBox.Size = new System.Drawing.Size(180, 20);
+            this.PunctuationBox.TabIndex = 4;
+            this.PunctuationBox.Text = ";:\"@#$%^&\t*(){}\\|,/<>`~[].?!";
+            // 
+            // FilenameLabel
+            // 
+            this.FilenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilenameLabel.Location = new System.Drawing.Point(25, 313);
+            this.FilenameLabel.Name = "FilenameLabel";
+            this.FilenameLabel.Size = new System.Drawing.Size(359, 15);
+            this.FilenameLabel.TabIndex = 6;
+            this.FilenameLabel.Text = "Waiting to Contextualize texts...";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.FileName = "Repeatalizer.csv";
+            this.saveFileDialog.Filter = "CSV Files|*.csv";
+            this.saveFileDialog.Title = "Please choose where to save your output";
+            // 
+            // FunctionWordTextBox
+            // 
+            this.FunctionWordTextBox.Location = new System.Drawing.Point(25, 61);
+            this.FunctionWordTextBox.Multiline = true;
+            this.FunctionWordTextBox.Name = "FunctionWordTextBox";
+            this.FunctionWordTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.FunctionWordTextBox.Size = new System.Drawing.Size(143, 241);
+            this.FunctionWordTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(45, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "List of Words to";
+            // 
+            // EncodingDropdown
+            // 
+            this.EncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EncodingDropdown.FormattingEnabled = true;
+            this.EncodingDropdown.Location = new System.Drawing.Point(204, 179);
+            this.EncodingDropdown.Name = "EncodingDropdown";
+            this.EncodingDropdown.Size = new System.Drawing.Size(180, 21);
+            this.EncodingDropdown.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Encoding of Text Files:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(227, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "( i.e., Word +/- N words )";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(45, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Contextualize";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(243, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "N = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(401, 336);
+            this.Controls.Add(this.WordWindowSizeTextbox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EncodingDropdown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FunctionWordTextBox);
+            this.Controls.Add(this.FilenameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PunctuationBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ScanSubfolderCheckbox);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(417, 375);
+            this.MinimumSize = new System.Drawing.Size(417, 375);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contextualizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WordWindowSizeTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.CheckBox ScanSubfolderCheckbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PunctuationBox;
+        private System.Windows.Forms.Label FilenameLabel;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox FunctionWordTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox EncodingDropdown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        //this is what runs at initialization
+        public Form1()
+        {
+
+            InitializeComponent();
+            FunctionWordTextBox.Text = Contextualizer.Properties.Resources.function_word_list.ToString();
+
+            foreach(var encoding in Encoding.GetEncodings())
+            {
+                EncodingDropdown.Items.Add(encoding.Name);
+            }
+            EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+
+
+        }
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (Convert.ToUInt32(WordWindowSizeTextbox.Text) < 1)
+            {
+                MessageBox.Show("Word Window Size must be >= 1.", "Problem with settings", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+            FolderBrowser.Description = "Please choose the location of your .txt files";
+            FolderBrowser.ShowDialog();
+            string TextFileFolder = FolderBrowser.SelectedPath.ToString();
+
+            if (TextFileFolder != "")
+            {
+
+                saveFileDialog.FileName = "Contextualizer.csv";
+
+                saveFileDialog.InitialDirectory = TextFileFolder;
+                saveFileDialog.ShowDialog();
+
+                
+                string OutputFileLocation = saveFileDialog.FileName;
+
+                if (OutputFileLocation != "") { 
+                    button1.Enabled = false;
+                    WordWindowSizeTextbox.Enabled = false;
+                    FunctionWordTextBox.Enabled = false;
+                    ScanSubfolderCheckbox.Enabled = false;
+                    PunctuationBox.Enabled = false;
+                    EncodingDropdown.Enabled = false;
+                    BgWorker.RunWorkerAsync(new string[] {TextFileFolder, OutputFileLocation});
+                }
+            } 
+
+        }
+
+        
+
+
+
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            int WordWindowSize = 2;
+
+            
+            //set up our sentence boundary detection
+            Regex NewlineClean = new Regex(@"[\r\n]+", RegexOptions.Compiled);
+
+            //selects the text encoding based on user selection
+            Encoding SelectedEncoding = null;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                SelectedEncoding = Encoding.GetEncoding(EncodingDropdown.SelectedItem.ToString());
+                WordWindowSize = Convert.ToInt32(WordWindowSizeTextbox.Text);
+            });
+
+            if (WordWindowSize < 1) WordWindowSize = 1;
+
+
+
+            //the very first thing that we want to do is set up our function word lists
+            List<string> WordWildcardList = new List<string>();
+            List<string> WordsToHash = new List<string>();
+            
+            string[] OriginalFunctionWordList = NewlineClean.Split(FunctionWordTextBox.Text.ToLower());
+            OriginalFunctionWordList = OriginalFunctionWordList.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+            foreach(string Word in OriginalFunctionWordList)
+            {
+                string WordToParse = Word.Trim();
+
+                if (WordToParse.Contains('*'))
+                {
+                    WordWildcardList.Add(WordToParse.Replace("*", ""));
+                }
+                else
+                {
+                    WordsToHash.Add(WordToParse);
+                }
+
+            }
+
+            //remove duplicates
+            WordWildcardList = WordWildcardList.Distinct().ToList();
+            WordsToHash = WordsToHash.Distinct().ToList();
+
+            HashSet<string> HashedWords = new HashSet<string>(WordsToHash);
+            string[] WordsWithWildCards = WordWildcardList.ToArray();
+
+            WordsToHash = null;
+            WordWildcardList = null;
+
+            int WildCardWordListLength = WordsWithWildCards.Length;
+
+
+            //get the list of files
+            var SearchDepth = SearchOption.TopDirectoryOnly;
+            if (ScanSubfolderCheckbox.Checked)
+            {
+                SearchDepth = SearchOption.AllDirectories;
+            }
+            var files = Directory.EnumerateFiles( ((string[])e.Argument)[0], "*.txt", SearchDepth);
+
+
+
+            try { 
+            using (StreamWriter outputFile = new StreamWriter(((string[])e.Argument)[1]))
+            {
+
+                string HeaderString = "\"Filename\",\"Observation\",\"Word\",\"Context\"";
+
+                outputFile.WriteLine(HeaderString);
+
+
+                foreach (string fileName in files)
+                {
+
+                    //set up our variables to report
+                    string Filename_Clean = Path.GetFileName(fileName);
+                    
+
+                    //report what we're working on
+                    FilenameLabel.Invoke((MethodInvoker)delegate
+                    {
+                        FilenameLabel.Text = "Analyzing: " + Filename_Clean;
+                    });
+
+
+                                            
+
+                    //do stuff here
+                    string readText = File.ReadAllText(fileName, SelectedEncoding).ToLower();
+
+
+                    readText = NewlineClean.Replace(readText, " ");
+
+                    //remove all the junk punctuation
+                    foreach (char c in PunctuationBox.Text)
+                    {
+                        readText = readText.Replace(c, ' ');
+                    }
+
+
+                    uint NumberOfMatches = 0;
+
+                    //splits everything out into words
+                    string[] Words = readText.Trim().Split(' ');
+                    Words = Words.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                        for (int i = 0; i < Words.Length; i++)
+                        {
+
+                            bool IsWordMatched = false;
+
+                            //this is where all of your magic is going to happen
+                            if (HashedWords.Contains(Words[i]))
+                            {
+                                IsWordMatched = true;
+                            }
+                            else
+                            {
+                                for (int j = 0; j < WildCardWordListLength; j++)
+                                {
+                                    if (Words[i].StartsWith(WordsWithWildCards[j]))
+                                    {
+                                        IsWordMatched = true;
+                                        break;
+                                    }
+                                }
+
+                            }
+
+
+                            if (IsWordMatched)
+                            {
+                                NumberOfMatches += 1;
+
+                                //create a new array that will contain the word window
+                                //string[] WordsInWindow = new string[1 + (WordWindowSize * 2)];
+                                int SkipAmount = i - WordWindowSize;
+                                int TakeAmount = (WordWindowSize * 2) + 1;
+
+                                if (SkipAmount < 0)
+                                {
+                                    TakeAmount += SkipAmount;
+                                    SkipAmount = 0;
+                                }
+
+                                if (i + WordWindowSize >= Words.Length)
+                                {
+                                    TakeAmount = Words.Length - SkipAmount;
+                                }
+
+
+                                
+                                var WordsInWindow = Words.Skip(SkipAmount).Take(TakeAmount).ToArray();
+
+
+
+                                //pull together the output
+                                string[] OutputString = new string[] {"", "", "", ""};
+                                OutputString[0] = "\"" + Filename_Clean + "\"";
+                                OutputString[1] = NumberOfMatches.ToString();
+                                OutputString[2] = "\"" + Words[i] + "\"";
+                                OutputString[3] = "\"" + String.Join(" ", WordsInWindow) + "\"";
+
+                                outputFile.WriteLine(String.Join(",", OutputString));
+                            }
+                            
+
+                        }
+
+                
+       
+
+                }
+
+
+            }
+
+            }
+            catch
+            {
+                MessageBox.Show("Contextualizer could not open your output file\r\nfor writing. Is the file open in another application?");
+            }
+
+
+
+            
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            WordWindowSizeTextbox.Enabled = true;
+            FunctionWordTextBox.Enabled = true;
+            ScanSubfolderCheckbox.Enabled = true;
+            PunctuationBox.Enabled = true;
+            EncodingDropdown.Enabled = true;
+            FilenameLabel.Text = "Finished!";
+            MessageBox.Show("Contextualizer has finished analyzing your texts.", "Analysis Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void WordWindowSizeTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void PhraseLengthTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void BigWordTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+
+    }
+    
+
+
+}
